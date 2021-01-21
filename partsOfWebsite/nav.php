70e6d4d74b10ccd3b2812b390d9c1c4595295144
ranum99 <aleksanderranum99@hotmail.com>
2021-01-22 00:07:11 +0100

    login og registrering knapper i <nav/> test av registrering overgang

--- partsOfWebsite/nav.php
+++ partsOfWebsite/nav.php
@@ -3,6 +3,9 @@
     <nav>
         <a href=""><img alt="Leap Glocal logo" src="../img/leap_logo_full.png"></a>
         <section>NAV ELEMENTER</section>
-        <section>PROFIL/LOGIN/REGISTER</section>
+        <section>
+            <a class="asButton" href="/beforeLogin/login.php">Logg inn</a>
+            <a class="asButton" href="/beforeLogin/register.php">Bli medlem</a>
+        </section>
     </nav>
     ';
