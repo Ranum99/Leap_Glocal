0658cf61b230bbe95cd60a9755a49cf3ea10ee08
ranum99 <aleksanderranum99@hotmail.com>
2021-02-23 18:02:40 +0100

    nav design

--- partsOfWebsite/nav.php
+++ partsOfWebsite/nav.php
@@ -8,29 +8,32 @@
             $linkForConsultant = '<a href="\skole\leap-glocal\contactConsultant\consultant\index.php">Mulige spørsmål</a>';
         }
         echo '
-        <nav>
-            <a href="/skole/leap-glocal"><img alt="Leap Glocal logo" src="/skole/leap-glocal/img/leap_logo_full.png"></a>
-            <section class="mainNavigation">
-                <a href="">Finn andre gründere</a>
-                <a href="">Gründerhubber/akseleratorer</a>
-                <a href="">Støtteordninger</a>
-                <a href="\skole\leap-glocal\contactConsultant\user\">Kontakt en konsulent</a>
-            </section>
-            <section>
-                <a class="asButton" href="/skole/leap-glocal/profile.php?user='.$hassedUserId.'">'.$_SESSION['userdata']->__get('name').'</a>
-            </section>
-        </nav>
-        
+        <div class="navDiv">
+            <nav>
+                <a href="/skole/leap-glocal"><img alt="Leap Glocal logo" src="/skole/leap-glocal/img/leap_logo_full.png"></a>
+                <section class="mainNavigation">
+                    <a href="">Finn andre gründere</a>
+                    <a href="">Gründerhubber/akseleratorer</a>
+                    <a href="">Støtteordninger</a>
+                    <a href="\skole\leap-glocal\contactConsultant\user\">Kontakt en konsulent</a>
+                </section>
+                <section>
+                    <a class="asButton" href="/skole/leap-glocal/profile.php?user='.$hassedUserId.'">'.$_SESSION['userdata']->__get('name').'</a>
+                </section>
+            </nav>
+        </div>
         ';
     } else {
         echo '
-        <nav>
-            <a href="/skole/leap-glocal"><img alt="Leap Glocal logo" src="/skole/leap-glocal/img/leap_logo_full.png"></a>
-            <section>NAV ELEMENTER</section>
-            <section>
-                <a class="asButton" href="/skole/leap-glocal/loginRegister/login.php">Logg inn</a>
-                <a class="asButton" href="/skole/leap-glocal/loginRegister/register.php">Bli medlem</a>
-            </section>
-        </nav>
+        <div class="navDiv">
+            <nav>
+                <a href="/skole/leap-glocal"><img alt="Leap Glocal logo" src="/skole/leap-glocal/img/leap_logo_full.png"></a>
+                <section>NAV ELEMENTER</section>
+                <section>
+                    <a class="asButton" href="/skole/leap-glocal/loginRegister/login.php">Logg inn</a>
+                    <a class="asButton" href="/skole/leap-glocal/loginRegister/register.php">Bli medlem</a>
+                </section>
+            </nav>
+        </div>
         ';
     }
