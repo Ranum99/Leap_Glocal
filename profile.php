7b406759a7cbfb7c92fa3bc767610b6dc4959095
AleksanderSandnes <62210498+AleksanderSandnes@users.noreply.github.com>
2021-03-26 16:47:59 +0100

    Designet ferdig profilsiden

--- profile.php
+++ profile.php
@@ -6,7 +6,7 @@
 
     $fillInRestOfDataBtn = "";
     if (getDataFromSessionColumn_userdata("requiredColumnsFilled") == 0)
-        $fillInRestOfDataBtn = '<a class="asButton" href="loginRegister/registerUser.php">Fyll inn manglende data</a>';
+        $fillInRestOfDataBtn = '<a href="loginRegister/registerUser.php" class="messageBtn">Fill in more data</a>';
 
     if (!isset($_GET['user']) && !empty($_GET['user']))
         goback();
@@ -54,19 +54,101 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="partsOfWebsite/css/profile.css">
     <link rel="stylesheet" type="text/css" href="global/common.css">
     <script src="https://kit.fontawesome.com/397d207bea.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/84fdcc201f.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <!-- HERE COMES <NAV/> FROM PHP FILE -->
     <?php include_once "partsOfWebsite/nav.php" ?>
 
     <main>
-        <?php if ($user == $_SESSION['userdata']) echo '<a class="asButton" href="backend/logout.php">Logg ut</a>' ?>
-        <?php echo $fillInRestOfDataBtn; ?>
-        <p>Email: <?php echo  $user->__get('email')?></p>
+        <div class="pageWrapper">
+            <div class="alignLeft">
+                <div class="profileCard">
+                    <img id="profilePicture" src="img/pb-test.jpg">
+                    <h2><?php echo $user->__get('name')?></h2>
+                    <p class="profileCardText">
+                        <?php echo $user->__get('description')?>
+                        <br><br>
+                        <?php echo $user->__get('address')?>, <?php echo $user->__get('country')?>
+                    </p>
+                    <?php echo $fillInRestOfDataBtn; ?>
+                    <?php if ($user == $_SESSION['userdata']) echo '<a href="backend/logout.php" class="logOutBtn">Log Out</a>' ?>
+                    <a href="/" class="messageBtn">Message</a>
+                </div>
+                <div class="links">
+                    <div class="firstMargin">
+                        <i class="fas fa-globe fa-lg"></i>
+                        <h6>Website</h6>
+                        <p class="alignToRight1"><?php echo $user->__get('websiteURL')?></p>
+                    </div>
+                    <div class="Margins">
+                        <i class="fab fa-twitter fa-lg" style="color: deepskyblue;"></i>
+                        <h6>Twitter</h6>
+                        <p class="alignToRight2"><?php echo $user->__get('twitter')?></p>
+                    </div>
+                    <div class="Margins">
+                        <i class="fab fa-instagram fa-lg" style="color: hotpink;"></i>
+                        <h6>Instagram</h6>
+                        <p class="alignToRight3"><?php echo $user->__get('todo')?></p>
+                    </div>
+                    <div class="Margins">
+                        <i class="fab fa-facebook-f fa-lg" style="color: dodgerblue"></i>
+                        <h6>Facebook</h6>
+                        <p class="alignToRight4"><?php echo $user->__get('facebook')?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="userInfo">
+                <div class="firstMargin">
+                    <h6>Full Name</h6>
+                    <p class="displayTop1"><?php echo $user->__get('name')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>Email</h6>
+                    <p class="displayTop2"><?php echo $user->__get('email')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>Phone</h6>
+                    <p class="displayTop3"><?php echo $user->__get('phoneNumber')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>Address</h6>
+                    <p class="displayTop4"><?php echo $user->__get('address')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>Organization Number</h6>
+                    <p class="displayTop5"><?php echo $user->__get('orgNumber')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>startup Phase</h6>
+                    <p class="displayTop6"><?php echo $user->__get('numOfEmp')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>Description</h6>
+                    <p class="displayTop7"><?php echo $user->__get('description')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>Looking for</h6>
+                    <p class="displayTop8"><?php echo $user->__get('lookingFor')?></p>
+                </div>
+                <hr>
+                <div class="Margins">
+                    <h6>Specification</h6>
+                    <p class="displayTop9"><?php echo $user->__get('specification')?></p>
+                </div>
+            </div>
+        </div>
     </main>
-
 
     <!-- HERE COMES <FOOTER/> FROM PHP FILE -->
     <?php include_once "partsOfWebsite/footer.php" ?>
