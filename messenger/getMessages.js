60d35b20a7707764c6dfb52ea4b92f8ae9bcbfe3
ranum99 <aleksanderranum99@hotmail.com>
2021-03-31 15:01:29 +0200

    samtale bugs

--- messenger/getMessages.js
+++ messenger/getMessages.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    let messagesDiv = document.getElementById('messagesHere');
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
     var intervalId = window.setInterval(function(){
         getMessages();
     }, 1001);
@@ -13,8 +15,10 @@ function getMessages() {
         url: 'getNewMessages.php',
         data: {conversation: 1},
         success: function(data){
-            theReturn = data;
-            updateMessages(data);
+            if (data !== null && data !== "") {
+                theReturn = data;
+                updateMessages(data);
+            }
         }
     });
 
@@ -23,4 +27,6 @@ function getMessages() {
 
 function updateMessages(data) {
     document.getElementById('messagesHere').innerHTML += data;
+    let messagesDiv = document.getElementById('messagesHere');
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
 }
